fc00671b338a8797fc33aedb41efcdd67b3892a0
yamini <yamineems@gmail.com>
2021-05-19 09:51:19 +0530

    Scroll Animation

---
+++ 6.Scroll_Animation/script.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll('.box')
+// scroll is a window object property
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        // getBoundingClientRect gives the dimensions ( x y top bottom axis) relatve to the viewport of the element
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}

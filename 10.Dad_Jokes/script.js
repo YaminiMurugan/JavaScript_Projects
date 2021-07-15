cf29ee520e6b52bbafb756945a1495cfc7c21141
yamini <yamineems@gmail.com>
2021-07-15 09:36:12 +0530

    Dad jokes - API fetch usage
    Used API fetch to get json resources through promises and asyn/ await

--- 10.Dad_Jokes/script.js
+++ 10.Dad_Jokes/script.js
@@ -1,32 +1,37 @@
-const jokeEle = document.getElementById('joke')
-const btn = document.getElementById('jokeBtn')
+const jokeEl = document.getElementById('joke')
+const btn = document.getElementById('jokebtn')
 
 generateJoke()
-btn.addEventListener('click' , generateJoke)
 
-//*******/ using Async/Await ********//
+// Creating a fetch request with promises 
+
+// function generateJoke(){
+//     const headerConf = {
+//         headers : {
+//             Accept:'application/json'
+//         },
+//     }
+
+//     fetch('https://icanhazdadjoke.com' , headerConf)
+//     .then(response => response.json()) //returns a promise
+//     .then(data => jokeEl.innerHTML = data.joke)
+// }
+
+// Creating a fetch request with asyn / await
 async function generateJoke(){
     const headerConf = {
         headers:{
             Accept:'application/json'
         },
     }
-    const response = await fetch('https://icanhazdadjoke.com' , headerConf)
-const data = await response.json()
-jokeEle.innerHTML=data.joke
-}
-// *******Using Promises .then() approach*******
-// function generateJoke (){
-//     const headerConf = {
-//         headers:{
-//             Accept:'application/json'
-//         },
-//     }
 
-//     fetch('https://icanhazdadjoke.com' , headerConf)
-//         // return's a promise from the https request made through fetch
-//         .then((response)=> response.json())
-//         .then((data) => jokeEle.innerHTML=data.joke)
-// }
+    const response = await fetch('https://icanhazdadjoke.com',headerConf)
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
+}
+
+btn.addEventListener('click' , generateJoke)
+
+
 
 
